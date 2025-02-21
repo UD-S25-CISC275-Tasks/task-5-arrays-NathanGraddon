36913728625cd01ec7f74b450189c4fa8fcc054e
Nathan <ngraddon@udel.edu>
2025-02-21 12:51:34 -0500

    taking for loops out, triple done

--- src/arrays.ts
+++ src/arrays.ts
@@ -23,11 +23,7 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    let newArr: number[] = [];
-    for (let num of numbers) {
-        newArr.push(num * 3);
-    }
-    return newArr;
+    return numbers.map((x) => x * 3);
 }
 
 /**
